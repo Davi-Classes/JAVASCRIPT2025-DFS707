4270ca00d707d1b82344528eb8cfca46c293705e
Davi Gomes Lucciola <69369398+davi-lucciola@users.noreply.github.com>
2025-08-23 10:26:30 -0300

    Update ex03.js

--- Aula 07/ex03.js
+++ Aula 07/ex03.js
@@ -1,1 +1,5 @@
 // 3 - Faça um programa onde você declare um array com 5 nomes, depois utilize a função "map" para criar um novo array onde todos os nomes devem estar em maiúsculo (.toUpperCase).
+const nomes = ['villena', 'millena', 'madalena', 'fernanda', 'luna']
+const nomesEmMaiusculo = nomes.map((nome) => nome.toUpperCase())
+
+console.log(nomesEmMaiusculo)
