cd8ae41571848265276df198cc2cd8a0c85dde50
Davi Gomes Lucciola <69369398+davi-lucciola@users.noreply.github.com>
2025-08-23 10:25:30 -0300

    Update ex05.js

--- Aula 07/ex05.js
+++ Aula 07/ex05.js
@@ -1,2 +1,5 @@
 // 5 - Dado o array de preços abaixo, faça um programa que crie um novo array utilizando o filter, onde ele deve trazer somente os preços maiores que 50.
 const precos = [12, 45, 76, 34, 65, 87, 88, 12, 344, 200, 445]
+
+const precosMaioresQue50 = precos.filter((preco) => preco > 50)
+console.log(precosMaioresQue50)
